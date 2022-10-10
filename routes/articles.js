bb090e620b73c72a4390a67569c4eb975f1c81cb
shyvhx <xinstylex@gmail.com>
2022-10-10 21:35:30 +0300

    Added edit functionality

--- routes/articles.js
+++ routes/articles.js
@@ -27,21 +27,10 @@ router.get('/:slug', async (req,res) => {
     
 })
 
-router.post('/', async (req,res) => {
-    let article = new Article({
-        title: req.body.title,
-        description: req.body.description,
-        markdown: req.body.markdown
-    })
-    try{
-        await article.save()
-        res.redirect(`/articles/${article.slug}`)
-    } catch (e){
-        res.render('articles/new', {article: article})
-        console.log(e)
-    }
-    
-})
+router.post('/', async (req,res,next) => {
+    req.article = new Article()
+    next()
+}, saveArticleAndRedirect('new'))
 
 
 router.delete('/:id', async (req,res) => {
@@ -50,15 +39,27 @@ router.delete('/:id', async (req,res) => {
 })
 
 
-router.put('/:id', (req,res) => {
-
-
-
-})
+router.put('/:id', async (req,res,next) => {
+    req.article = await Article.findById(req.params.id)
+    next()
+}, saveArticleAndRedirect('new'))
 
 
 function saveArticleAndRedirect(path){
-
+    return async (req,res) => {
+        let article = req.article
+        article.title= req.body.title
+        article.description= req.body.description
+        article.markdown= req.body.markdown
+        
+        try{
+            article = await article.save()
+            res.redirect(`/articles/${article.slug}`)
+        } catch (e){
+            res.render(`articles/${path}`, {article: article})
+            console.log(e)
+        }
+    }
 
     
 }
